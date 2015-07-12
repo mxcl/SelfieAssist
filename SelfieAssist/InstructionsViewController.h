ac3aeda22395e049f893fa69a1db90e7e994759d
joe <jdivittorio3@gmail.com>
2015-07-12 10:56:51 -0500

    instructions view controller

---
+++ SelfieAssist/InstructionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  InstructionsViewController.h
+//  SelfieAssist
+//
+//  Created by Joe on 7/12/15.
+//  Copyright (c) 2015 Max Howell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InstructionsViewController : UIViewController
+
+@end
